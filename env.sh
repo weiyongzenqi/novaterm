27536941b12d7828443d40640b10cfe9db990ccc
weiyongzenqi <67868002+weiyongzenqi@users.noreply.github.com>
2026-06-05 15:53:33 +0800

    移除 env.sh，添加到 .gitignore
    env.sh 包含本地路径信息，不应提交到仓库

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# NovaTerm 环境配置
-export PATH="/home/lighthouse/.node-install/bin:/home/lighthouse/.cargo/bin:$PATH"
-export CARGO_HOME="/home/lighthouse/.cargo"
-export RUSTUP_HOME="/home/lighthouse/.rustup"
